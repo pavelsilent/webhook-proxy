ba82e0cee6edee3877f0a2147fa155584381cd5d
p.brichev <p.brichev@sis-it.pro>
2020-10-17 18:01:12 +0700

    unproxy sender

--- src/main/java/pro/sisit/utils/webhookproxy/service/sender/SenderService.java
+++ src/main/java/pro/sisit/utils/webhookproxy/service/sender/SenderService.java
@@ -1,11 +1,11 @@
 package pro.sisit.utils.webhookproxy.service.sender;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import pro.sisit.utils.webhookproxy.domain.entity.Target;
 import pro.sisit.utils.webhookproxy.domain.model.telegram.Message;
-
-import java.util.List;
+import pro.sisit.utils.webhookproxy.util.HibernateUtil;
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +16,13 @@ public class SenderService {
     @SuppressWarnings("unchecked")
     public <T extends Target, R> R send(T target, Message message) {
         return (R) senders.stream()
-                .filter(sender -> sender.supports(target))
-                .findFirst()
-                .map(sender -> sender.send(target, message))
-                .orElseThrow(() -> new RuntimeException(
-                        String.format("Not found sender for target '%s'.",
-                                target.getClass().getSimpleName())));
+                          .map(HibernateUtil::unProxyObject)
+                          .filter(sender -> sender.supports(target))
+                          .findFirst()
+                          .map(sender -> sender.send(target, message))
+                          .orElseThrow(() -> new RuntimeException(
+                              String.format("Not found sender for target '%s'.",
+                                  target.getClass().getSimpleName())));
     }
 
 
